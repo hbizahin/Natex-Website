d7a207828b4298a29660d2619fab67b5c4ace824
hbizahin <hbizahin@gmail.com>
2024-08-15 16:53:59 +0600

    Home and Contact page added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  module.exports = {
+    //...
+    daisyui: {
+      themes: ["light", "dark", "corporate"],
+    },
+  }
+
+// tailwind.config.js (contacts)
+module.exports = {
+    plugins: [
+
+      require('@tailwindcss/forms'),
+    ],
+  }
